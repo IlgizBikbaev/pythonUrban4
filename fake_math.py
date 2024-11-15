fb5dca953feef6d962c3db6f22e08719c12590cb
IlgizBikbaev <131395377+IlgizBikbaev@users.noreply.github.com>
2024-11-15 14:42:52 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+        if second == 0:
+            message = print("Ошибка ввода. На ноль делить нельхя")
+            return message
+        else:
+            return print(first / second)
+
+
+#res1 = fake_divide(69, 3)
+#res2 = fake_divide(3, 0)
+
---
+++ true_math.py
@@ -0,0 +1,11 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        print(inf)
+    else:
+        return print(first / second)
+
+
+#result3 = true_divide(49, 7)
+#result4 = true_divide(15, 0)
